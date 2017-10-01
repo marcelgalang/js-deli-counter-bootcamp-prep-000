8f622c723aad01dbb6b21c1d8afd5e4ea4c768e0
Ronil Bhatia <ronilbhatia@gmail.com>
2017-10-01 21:40:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+
+var array = ['Joe']
+console.log(array)
+function addPerson(katzDeli, b) {
+  katzDeli = [...katzDeli, b]
+  console.log("Welcome, " + b + ". You are number " + (katzDeli.length) + " in line.")
+  console.log(katzDeli)
+}
+
+addPerson(array, 'Fred')
+
 var katzDeli = [];
-function takeANumber(katzDeliLine, b) {
-  katzDeliLine = [...katzDeliLine, b]
+function takeANumber(katzDeliLine, newPerson) {
+  katzDeliLine = [...katzDeliLine, newPerson]
   var length = katzDeliLine.length
-  return "Welcome, " + b + ". You are number " + length + " in line."
+  return "Welcome, " + newPerson + ". You are number " + length + " in line."
 }
