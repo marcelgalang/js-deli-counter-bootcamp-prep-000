1c8441cb3e696fae8868b215b706d0aca10385ad
Ronil Bhatia <ronilbhatia@gmail.com>
2017-10-01 21:17:16 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,6 @@
+var katzDeli = []
+function takeANumber(katzDeliLine, newPerson) {
+  katzDeliLine = [...katzDeliLine, newPerson]
+  return "Welcome, " + katzDeliLine[katzDeliLine.length-1] + ". You are number " + (katzDeliLine.length) + " in line."
+  return katzDeli
+}
