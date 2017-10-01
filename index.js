0fd47dded123e6014a23614f4a4964dbd689e685
Ronil Bhatia <ronilbhatia@gmail.com>
2017-10-01 21:28:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var array = ['Joe']
 console.log(array)
 function addPerson(a, b) {
   a = [...a, b]
-  console.log("Welcome, " + b + ". You are number" + (a.length) + " in line.")
+  console.log("Welcome, " + b + ". You are number " + (a.length) + " in line.")
 }
 
 addPerson(array, 'Fred')
